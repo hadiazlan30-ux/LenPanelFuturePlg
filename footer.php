3a4281481959723821bb292834b22e72bf75d486
hadiazlan30-ux <hadiazlan30@gmail.com>
2025-11-13 09:51:31 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> PKM Ransum. Hak Cipta Dilindungi.</p>
-            <p class="team-credit">Dikembangkan oleh: Azlan, Geu, Devi, dan Fadel</p>
-        </div>
-    </footer>
-
-</body>
-</html>
